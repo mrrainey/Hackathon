27ac89095af85aac77c3912e1aad79916e561f08
Mark Spiers <marksp@kainos.com>
2019-10-26 18:50:28 +0100

    restart

--- Train_model.py
+++ Train_model.py
@@ -1,33 +1,8 @@
 import os
 import pandas
-
-from imageseg import ImageLoader
-from mrcnn.config import Config
-import mrcnn.model as modellib
+import skimage.
 
 model_save = os.getcwd() + "/save"
-
-
-class LesionsConfig(Config):
-    NAME = "lesions"
-
-    NUM_CLASSES = 2
-
-    IMAGE_MIN_DIM = 64
-    IMAGE_MAX_DIM = 64
-
-    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
-
-    TRAIN_ROIS_PER_IMAGE = 32
-
-    STEPS_PER_EPOCH = 100
-
-    VALIDATION_STEPS = 5
-
-    LEARNING_RATE = 0.002
-
-
-config = LesionsConfig()
 
 # set up training data
 
@@ -41,20 +16,5 @@ del data
 train_data.drop(columns="subset")
 val_data.drop(columns="subset")
 
-dataset_train = ImageLoader()
-dataset_train.load_image_init(train_data)
-dataset_train.prepare()
+def image_loader(image_id):
 
-dataset_val = ImageLoader()
-dataset_val.load_image_init(val_data)
-dataset_val.prepare()
-
-# Create model in training mode
-model = modellib.MaskRCNN(mode="training", config=config,
-                          model_dir=model_save)
-model.load_weights(model.get_imagenet_weights(), by_name=True)
-
-model.train(dataset_train, dataset_val,
-            learning_rate=config.LEARNING_RATE,
-            epochs=1,
-            layers='all')
--- imageseg.py
+++ imageseg.py
@@ -1,15 +1,15 @@
-import os
-from mrcnn import utils
-
-
-class ImageLoader(utils.Dataset):
-
-    def load_image_init(self, data_url):
-        self.add_class("lesion", 1, "lesion")
-
-        for image in data_url['image']:
-            self.add_image(
-                "lesion",
-                image_id=image,
-                path=os.path.join(os.getcwd(), "image/{}.png".format(image)))
+# import os
+# from mrcnn import utils
+#
+#
+# class ImageLoader(utils.Dataset):
+#
+#     def load_image_init(self, data_url):
+#         self.add_class("lesion", 1, "lesion")
+#
+#         for image in data_url['image']:
+#             self.add_image(
+#                 "lesion",
+#                 image_id=image,
+#                 path=os.path.join(os.getcwd(), "image/{}.png".format(image)))
 
