34bcfd4fc7bd8ffdec6fcc5857361c2bc8c80740
mrrainey <mrainey866@gmail.com>
2019-10-26 16:07:23 +0100

    load_image now returns numpy tensor

--- imageseg.py
+++ imageseg.py
@@ -3,24 +3,26 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 from mrcnn import utils
+from keras_preprocessing.image import img_to_array
 
 class ImageLoader(utils.Dataset):
 
-    def load_image(self, filename):
+    def load_image(self, filename, dir='image'):
         """Load in an image of the given filename
         """
         if dir == 'train':
-            img = Image.open(f"train/{filename}")
+            img = Image.open(f"train/{dir}/{filename}")
         elif dir == 'test':
-            img = Image.open(f"test/{filename}")
+            img = Image.open(f"test/{dir}/{filename}")
         else:
             raise Exception('Image was not found')
-        return img
+        
+        return img_to_array(img)
 
     def load_mask(self, filename):
         """Load masks of the given filename.
         """
-        return self.load_image(filename)
+        return self.load_image(filename, dir='mask')
 
 img_loader = ImageLoader()
 image = img_loader.load_image('1.png')
