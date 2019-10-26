7e90e45728e9be9cdfe0dc44f042d22dffc79e84
mrrainey <mrainey866@gmail.com>
2019-10-26 15:10:27 +0100

    load images and masks

---
+++ imageseg.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import numpy as np
+from PIL import Image
+from keras_preprocessing.image import img_to_array 
+
+class ImageLoader:
+
+    def load_image(self, filename, mask=False, dir='train'):
+        """Load in an image
+        """
+        if dir == 'train':
+            img = Image.open(f"train/{filename}")
+        elif dir == 'test':
+            img = Image.open(f"test/{filename}")
+        else:
+            raise Exception('Image was not found')
+        return img
+
+    def load_mask(self, filename):
+        """Generate instance masks for shapes of the given image ID.
+        """
+        return self.load_image(filename, mask=True)
+
+img_loader = ImageLoader()
+image = img_loader.load_image('1.png')
+
+mask = img_loader.load_mask('1.png')
+print(mask)
