878b5aa01d68449d42cdd62d28eccfcc6fb1ed34
Mark Spiers <marksp@kainos.com>
2019-10-26 15:19:22 +0100

    created model

---
+++ Train_model.py
@@ -0,0 +1,42 @@
+import os
+import pandas
+
+from mrcnn.config import Config
+import mrcnn.model as modellib
+
+model_save = os.getcwd() + "/save"
+
+
+class LesionsConfig(Config):
+    NAME = "lesions"
+
+    NUM_CLASSES = 2
+
+    IMAGE_MIN_DIM = 64
+    IMAGE_MAX_DIM = 64
+
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+
+    TRAIN_ROIS_PER_IMAGE = 32
+
+    STEPS_PER_EPOCH = 100
+
+    VALIDATION_STEPS = 5
+
+
+config = LesionsConfig()
+
+# set up training data
+
+data = pandas.read_csv('train_data_64/index.csv')
+
+data = data.sample(frac=1, random_state=42)
+train_data = data.loc[data['subset'] == 'T']
+test_data = data.loc[data['subset'] == 'V']
+
+del data
+
+# Create model in training mode
+model = modellib.MaskRCNN(mode="training", config=config,
+                          model_dir=model_save)
+
